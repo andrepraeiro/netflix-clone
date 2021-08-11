0c9432996b9aa177138bde046a44ec6898b50811
Andre Praeiro <andrepraeiro@gmail.com>
2021-08-11 16:41:42 -0400

    inital commit

---
+++ public/js/api.js
@@ -0,0 +1,8 @@
+const api_key = ''
+
+const img_url = 'https://image.tmdb.org/t/p/w500'
+const genres_list_http = 'https://api.themoviedb.org/3/genre/movie/list?'
+const movie_genres_http = 'https://api.themoviedb.org/3/discover/movie?'
+const original_img_url = "https://image.tmdb.org/t/p/original"
+const movie_detail_http = "https://api.themoviedb.org/3/movie"
+
